27baab9741725c4b0d7dabd6a24507ad8db42682
LWC421 <lwc421@gmail.com>
2022-10-22 21:25:51 +0900

    [docs]controller에 swagger추가

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Logger, Post } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
 import { AuthService } from 'src/auth/auth.service';
 import { UserLoginDto } from './dtos/req/user-login.dto';
 import { UserResiterDTO } from './dtos/user-register.dto';
@@ -13,11 +14,13 @@ export class UsersController {
     private authService: AuthService,
   ) {}
 
+  @ApiOperation({ summary: '회원가입' })
   @Post()
   async signup(@Body() userRegisterDTO: UserResiterDTO) {
     return await this.usersService.registerUser(userRegisterDTO);
   }
 
+  @ApiOperation({ summary: '로그인' })
   @Post('login')
   async login(@Body() userLoginDTO: UserLoginDto) {
     return this.authService.jwtLogin(userLoginDTO);
