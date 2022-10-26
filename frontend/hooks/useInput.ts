84a0fb0e4b89dd97e8f870108c0a11fe4f92d699
LWC421 <lwc421@gmail.com>
2022-10-27 04:25:58 +0900

    [style]useInput 타입 추가

--- frontend/hooks/useInput.ts
+++ frontend/hooks/useInput.ts
@@ -1,19 +1,19 @@
-import { useCallback, useState } from "react";
+import { ChangeEvent, useCallback, useState } from "react";
 
 /**
  * onChange함수에 사용될 handler를 생성하기 위한 함수
  * @param {T} initialValue 초기값
- * @param {(e: any) => T} callback 타입<T>를 반환하는 onChange함수
- * @returns {[<T>, (any) => void, (<T>) => void]} [value, onChange, setValue]
+ * @param {(e: ChangeEvent<HTMLInputElement>) => T} callback 타입<T>를 반환하는 onChange함수, 생략시 e.target.value를 return
+ * @returns {[<T>, (ChangeEvent<HTMLInputElement>) => void, (<T>) => void]} [value, onChange, setValue]
  */
 const useInput = <T>(
   initialValue: T,
-  callback?: (e: any) => T
-): [T, (e: any) => void, (value: T) => void] => {
+  callback?: (e: ChangeEvent<HTMLInputElement>) => T
+): [T, (e: ChangeEvent<HTMLInputElement>) => void, (value: T) => void] => {
   const [value, setValue] = useState<T>(initialValue);
 
   const onChange = useCallback(
-    (e: any) => {
+    (e: ChangeEvent<HTMLInputElement>) => {
       const targetValue = callback
         ? callback(e)
         : (e.target.value as unknown as T);
