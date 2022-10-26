382c578657e1b6631fc3b4ef738d984d3e2a0ded
LWC421 <lwc421@gmail.com>
2022-10-27 04:23:32 +0900

    [feat]warnign 제거

--- frontend/hooks/useInput.ts
+++ frontend/hooks/useInput.ts
@@ -12,12 +12,15 @@ const useInput = <T>(
 ): [T, (e: any) => void, (value: T) => void] => {
   const [value, setValue] = useState<T>(initialValue);
 
-  const onChange = useCallback((e: any) => {
-    const targetValue = callback
-      ? callback(e)
-      : (e.target.value as unknown as T);
-    setValue(targetValue);
-  }, []);
+  const onChange = useCallback(
+    (e: any) => {
+      const targetValue = callback
+        ? callback(e)
+        : (e.target.value as unknown as T);
+      setValue(targetValue);
+    },
+    [callback]
+  );
 
   return [value, onChange, setValue];
 };
