a6f796c822aeea267cbb50c10ccba0418b6fba7e
LWC421 <lwc421@gmail.com>
2022-10-28 07:02:05 +0900

    [feat]Header 링크 추가

--- frontend/components/layout/Header.tsx
+++ frontend/components/layout/Header.tsx
@@ -1,9 +1,12 @@
+import { useRouter } from "next/router";
 import * as HeaderSt from "./Header.style";
 
 const Header = () => {
+  const router = useRouter();
+
   return (
     <HeaderSt.Header>
-      <HeaderSt.Logo>BTC</HeaderSt.Logo>
+      <HeaderSt.Logo onClick={() => router.push("/")}>BTC</HeaderSt.Logo>
     </HeaderSt.Header>
   );
 };
