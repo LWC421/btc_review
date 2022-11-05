a280731262f28399bb0532cfd491e613e6206095
LWC421 <lwc421@gmail.com>
2022-11-06 02:23:09 +0900

    [fix]Type변경

--- frontend/hooks/useInput.ts
+++ frontend/hooks/useInput.ts
@@ -3,7 +3,7 @@ import { ChangeEvent, useCallback, useState } from "react";
 /**
  * onChange함수에 사용될 handler를 생성하기 위한 함수
  * @param {T} initialValue 초기값
- * @param {(e: ChangeEvent<HTMLInputElement>) => T} callback 타입{T}를 반환하는 onChange함수, 생략시 e.target.value를 return
+ * @param {e: ChangeEvent<HTMLInputElement> => T} callback 타입{T}를 반환하는 onChange함수, 생략시 e.target.value를 return
  * @param {T} initialValid 초기valid값
  * @param {(value: T) => boolean} validation 타입{T}를 입력으로 하여 boolean값을 반환하는 validation 함수
  * @returns {[<T>, (ChangeEvent<HTMLInputElement>) => void, (<T>) => void, boolean, (boolean) => void]} [value, onChange, setValue, isValid, setIsValid]
