1b1661a656147ded356dd29e123092edc82a71de
LWC421 <lwc421@gmail.com>
2022-11-10 13:38:44 +0900

    [style]hover시 색상 따로 추가

--- frontend/styles/theme.ts
+++ frontend/styles/theme.ts
@@ -3,6 +3,8 @@ const theme = {
     primary: "rgb(79, 160, 149)",
     secondary: "rgb(175, 175, 175)",
     normal: "rgb(255, 255, 255)",
+    inverse: "rgb(255, 255, 255)",
+    inverseBackground: "rgb(50, 50, 50)",
   },
   shadow: {
     secondary: "0px 1px 4px rgba(0, 0, 0, 0.15)",
