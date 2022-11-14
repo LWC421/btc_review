35ec7dfab6986a1126cc1d800fdba760adf188bb
LWC421 <lwc421@gmail.com>
2022-11-14 17:35:47 +0900

    [fix]api분리되어있던거 합치기

--- frontend/hooks/useAuth.ts
+++ frontend/hooks/useAuth.ts
@@ -1,33 +1,55 @@
-import myInfoRequest, { MyInfoResponse } from "api/myInfoRequest";
-import axios from "axios";
 import { useState } from "react";
 import { useQuery } from "react-query";
 import { User } from "types";
+import customAxios from "utils/customAxios";
+import { getToken } from "utils/token";
+
+type MyInfoResponse = {
+  email: string;
+  nickname: string;
+};
 
 /**
  * 로그인이 된 경우인지 판별합니다
  * @returns {[boolean, User | null, boolean]} [로그인여부, user정보, 로딩여부]
  */
 const useAuth = (): [boolean, User | null, boolean] => {
+  const accessToken = getToken();
+
+  if (!accessToken) {
+    //accessToken이 없을 경우 바로 반환
+    return [false, null, false];
+  }
+
   const [isAuth, setIsAuth] = useState<boolean>(false);
   const [user, setUser] = useState<User | null>(null);
 
-  const { isLoading } = useQuery("myInfo", myInfoRequest, {
-    refetchOnWindowFocus: false,
-    retry: 0,
-    onSuccess: (data: MyInfoResponse) => {
-      setUser(data);
-      setIsAuth(true);
+  //기존 헤더의 token삭제 후 accessToken 교체
+  delete customAxios.defaults.headers.common.Authorization;
+  customAxios.defaults.headers.common[
+    "Authorization"
+  ] = `Bearer ${accessToken}`;
+
+  const { isLoading } = useQuery<MyInfoResponse>(
+    "myInfo",
+    async () => {
+      const res = await customAxios.get<MyInfoResponse>("/users");
+
+      return res.data;
     },
-    onError: (error) => {
-      //정보 가져오기 실패
-      if (axios.isAxiosError(error)) {
+    {
+      refetchOnWindowFocus: false,
+      retry: 0,
+      onSuccess: (data) => {
+        setUser(data);
+        setIsAuth(true);
+      },
+      onError: () => {
+        //정보 가져오기 실패
         setIsAuth(false);
-      } else {
-        setIsAuth(false);
-      }
-    },
-  });
+      },
+    }
+  );
 
   return [isAuth, user, isLoading];
 };
