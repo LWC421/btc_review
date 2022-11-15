067792d8551e593612d16ea1bbab4a393993da62
LWC421 <lwc421@gmail.com>
2022-11-15 16:19:39 +0900

    [feat]공통에러처리

--- frontend/api/getError.ts
+++ frontend/api/getError.ts
@@ -2,11 +2,20 @@ import axios from "axios";
 
 const getError = (error: unknown) => {
   if (axios.isAxiosError(error)) {
-    return error.response?.data.message ?? "에러 발생";
+    const message = error.response?.data.message;
+    if (message instanceof Array) {
+      //에러가 여러개일 경우 그 중 첫번째만
+      return message[0];
+    }
+    if (message) {
+      //문자열일 경우
+      return message;
+    }
+    return "에러 발생";
   } else if (error instanceof Error) {
     return error.message;
   } else {
-    return "에러";
+    return "에러 발생";
   }
 };
 
