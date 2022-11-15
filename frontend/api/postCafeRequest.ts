6954d7bbeb2d8d99d6ccc529653c7aa0c4e46c79
LWC421 <lwc421@gmail.com>
2022-11-15 16:20:15 +0900

    [feat]카페정보 생성 요청 기능

--- frontend/api/postCafeRequest.ts
+++ frontend/api/postCafeRequest.ts
@@ -1,12 +1,40 @@
+import { AxiosError } from "axios";
+import { useToast } from "hooks";
+import { useMutation } from "react-query";
 import customAxios from "utils/customAxios";
 import getError from "./getError";
 
 const postCafeRequest = (formData: FormData) => {
-  try {
-    customAxios.post("/", formData);
-  } catch (error) {
-    throw new Error(getError(error));
-  }
+  const pushToast = useToast();
+
+  const request = async () => {
+    await customAxios.post<void>("/cafes", formData, {
+      headers: { "Content-Type": "multipart/form-data" },
+    });
+  };
+
+  const { mutate, isLoading, isSuccess } = useMutation<
+    void,
+    AxiosError | Error
+  >(request, {
+    onSuccess: () => {
+      pushToast({
+        type: "success",
+        message: "카페 정보 기입 성공",
+        time: 2000,
+      });
+    },
+    onError: (error) => {
+      const errorMessage = getError(error);
+      pushToast({
+        type: "error",
+        message: errorMessage,
+        time: 2000,
+      });
+    },
+  });
+
+  return { mutate, isLoading, isSuccess };
 };
 
 export default postCafeRequest;
